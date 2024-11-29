4cb60192e08b58bc433dd9838af3715ec8eb9fe1
Yosita satiman <yosita_satiman@cmu.ac.th>
2024-11-29 14:18:06 +0700

    lab6

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i = 1;
+    int N = 0;
+    int x = 0;
+    int y = 0;
+    while(i != 0){
+        cout << "Enter an integer: ";
+        cin >> i;
+        N++;
+        if(i%2 == 0){
+            x++;
+        }else{
+            y++;
+        }
+    }
+    cout << "#Even numbers = " << x-1;
+    cout << "\n#Odd numbers = " << y;
     return 0;
 }
